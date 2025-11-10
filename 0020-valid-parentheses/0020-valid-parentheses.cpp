eb4a78e32fcbf15aba4fac480100afeaf7117234
Anish Mishra <2k23.cscys2310534@gmail.com>
2025-11-10 06:05:52 +0530

    Time: 0 ms (100%), Space: 8.8 MB (66.17%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for( auto c:s)
+        {
+            if(c == '('|| c == '{'|| c == '[')
+            st.push(c);
+            else if(c == ')' || c== ']' || c == '}')
+            {
+                if(st.empty()) return false;
+                if ((c == ')' && st.top() == '(') || 
+                (c == ']' && st.top() == '[') ||
+                ( c == '}' && st.top() == '{'))
+                st.pop();
+                else
+                return false;
+                
+            }
+        }
+        if(st.empty()) return true;
+        else return false;
+    }
+};
