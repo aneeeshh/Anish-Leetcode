dcd5de92cbb0bacc48dabbda34517b4324a1f562
Anish Mishra <2k23.cscys2310534@gmail.com>
2026-02-27 21:58:27 +0530

    Time: 15 ms (20.39%), Space: 14.4 MB (32.7%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+        for(char ch : s){
+                if( !st.empty() && ch == st.top()){
+                    st.pop();
+                }
+            else {
+                st.push(ch);
+            }
+        }
+        string result = "";
+        while(!st.empty()){
+            result += st.top();
+            st.pop();
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
