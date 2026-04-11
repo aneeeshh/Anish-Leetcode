a1100c98419f45fb4b6ce924679d5af08a45f073
Anish Mishra <2k23.cscys2310534@gmail.com>
2026-04-11 17:29:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    int find(vector<int> &a)
-    {
-        int maxc=-1;
-        for(int i=0;i<256;i++)
-        maxc=max(maxc,a[i]);
-        return maxc;
-    }
-
     int characterReplacement(string s, int k) {
-        int n=s.size();
-       vector<int> f(256,0);
-       int low=0,high=0,res=INT_MIN;
-       for(high=0;high<n;high++)
-       {
-           f[s[high]]++;
-           int maxcnt=find(f);
-           int len=high-low+1;
-           int diff=len-maxcnt;
-           while(diff>k)
-           {
-               f[s[low]]--;
-               low++;
-               maxcnt=find(f);
-               len=high-low+1;
-               diff=len-maxcnt;
-           }
-           len=high-low+1;
-           res=max(res,len);
-       }
-       return res;
+        int low = 0;
+        int n = s.size();
+        int res = 0;
+        
+        vector<int> f(26, 0);
+        int maxFreq = 0;
+
+        for(int high = 0; high < n; high++){
+            f[s[high] - 'A']++;
+            
+            // track max frequency
+            maxFreq = max(maxFreq, f[s[high] - 'A']);
+            
+            int len = high - low + 1;
+            
+            // shrink window if invalid
+            while(len - maxFreq > k){
+                f[s[low] - 'A']--;
+                low++;
+                len = high - low + 1;
+            }
+            
+            res = max(res, len);
+        }
+        
+        return res;
     }
 };
